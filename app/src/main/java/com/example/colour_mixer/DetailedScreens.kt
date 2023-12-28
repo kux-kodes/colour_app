5af7537f3a9d09d015215220e19ce7c882be21f3
kux-kodes <119114087+kux-kodes@users.noreply.github.com>
2023-12-29 00:13:29 +0200

    added vertical scrolling to red and blue details

--- app/src/main/java/com/example/colour_mixer/DetailedScreens.kt
+++ app/src/main/java/com/example/colour_mixer/DetailedScreens.kt
@@ -1,26 +1,32 @@
 package com.example.colour_mixer
 
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Red_details(navController: NavHostController){
     Box(modifier = Modifier
-        .width(200.dp)
-        .height(200.dp)
+        .width(400.dp)
+        .height(400.dp)
         .padding(20.dp)
-        .clip(RoundedCornerShape(20.dp))){
+        .clip(RoundedCornerShape(20.dp))
+        .verticalScroll(rememberScrollState())){
         Text(modifier = Modifier.padding(10.dp),
             text = "Passion and Love: Red is often associated with strong emotions, particularly love and passion. It is a color commonly used to represent romantic feelings.\n" +
                 "Energy and Power: Red is dynamic and attention-grabbing, symbolizing energy, power, and vitality.\n" +
@@ -31,14 +37,15 @@ fun Red_details(navController: NavHostController){
 @Composable
 fun Blue_details(navController:NavHostController){
     Box(modifier = Modifier
-        .width(200.dp)
-        .height(300.dp)
+        .width(400.dp)
+        .height(400.dp)
         .padding(20.dp)
-        .clip(RoundedCornerShape(20.dp)),
+        .clip(RoundedCornerShape(20.dp))
+        .verticalScroll(rememberScrollState()),
         contentAlignment = Alignment.Center){
         Text(modifier = Modifier.padding(10.dp),
-            text = "Calmness and Serenity: Blue is often associated with feelings of calmness, tranquility, and serenity. It is a color that can evoke a sense of peace and relaxation.\n" +
-                    "Trust and Stability: Blue is commonly used to convey trustworthiness, stability, and reliability. Many corporate logos and uniforms incorporate shades of blue to inspire confidence.\n" +
+            text = "Calmness and Serenity: Blue is often associated with feelings of calmness, tranquility, and serenity. It is a color that can evoke a sense of peace and relaxation.\n" +"\n"+
+                    "Trust and Stability: Blue is commonly used to convey trustworthiness, stability, and reliability. Many corporate logos and uniforms incorporate shades of blue to inspire confidence.\n" + "\n"+
                     "Royalty and Nobility: Historically, certain shades of blue were associated with royalty and nobility, as the pigments used to create them were rare and expensive.")
 
     }
